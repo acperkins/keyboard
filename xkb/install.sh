1e45569cf3fa833e56d27110cec6b8ef3a1d9e56
Anthony Perkins <anthony@acperkins.com>
2024-03-06 11:12:50 +0000

    Add keymap file to install script

--- xkb/install.sh
+++ xkb/install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/keymap"
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/rules"
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/symbols"
 cp ./rules/evdev "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/rules/evdev"
 cp ./rules/evdev.xml "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/rules/evdev.xml"
 cp ./symbols/acp "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/symbols/acp"
+setxkbmap -layout acp -print | tee "${XDG_CONFIG_HOME:-$HOME/.config}/xkb/keymap/acp"
+
+# Load the layout in X11 if needed with:
+#   xkbcomp -w0 -I"${XDG_CONFIG_HOME:-$HOME/.config}/xkb" -R"${XDG_CONFIG_HOME:-$HOME/.config}/xkb" keymap/acp $DISPLAY
